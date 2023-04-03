e8519fc03109f1c7293b5838966ed8a073250c29
Isaias Buscarino <isaias.buscarino@hotmail.com>
2023-04-03 23:59:59 +0200

    Create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <div>Main</div>
+import { GetStaticProps } from 'next'
+
+import HomeTemplate from 'templates/Home'
+import { initializeApollo } from 'utils/apollo'
+import { QUERY_CHARACTERS } from 'graphql/queries/characters'
+import { QueryCharacters } from 'graphql/generated/QueryCharacters'
+
+const apolloClient = initializeApollo()
+
+export default function HomePage() {
+  return <HomeTemplate />
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  await apolloClient.query<QueryCharacters>({
+    query: QUERY_CHARACTERS,
+    variables: { page: 1 }
+  })
+
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract()
+    }
+  }
 }
